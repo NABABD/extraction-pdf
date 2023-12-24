c693c04f7133eaae2dc9e41081a0f471509eba43
NABABD <nababd47@gmail.com>
2023-12-24 15:57:19 +0100

    ajout de fitz

--- convertiseur.py
+++ convertiseur.py
@@ -1,18 +1,7 @@
 import fitz
-import camelot
-"""pdf_path="PDF2.pdf"
-document=fitz.open(pdf_path)
-page_number=0
-page=document.load_page(page_number)
-text=page.get_text("text")
-print(text)
-document.close()
-"""
 pdf_path = "PDF2.pdf"
-
-# Utilisez la fonction read_pdf pour extrsaire les tableaux
-tables = camelot.read_pdf(pdf_path, flavor='stream', pages='all')
-
-# Affichez les tables extraites
-for table in tables:
-    print(table.df)
+doc = fitz.open(pdf_path)
+page_number = 0  # Numéro de la page (commence à 0)
+page = doc.load_page(page_number)
+text = page.get_text("text")
+print(text)
