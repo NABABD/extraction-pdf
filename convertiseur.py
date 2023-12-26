c163e6c48299ed7d83653bdda2f7ad37106bfd65
NABABD <nababd47@gmail.com>
2023-12-26 11:43:17 +0100

    ajout des étapes dans Trace_ecrite

--- convertiseur.py
+++ convertiseur.py
@@ -2,7 +2,7 @@ import fitz  # PyMuPDF
 
 pdf_path = "PDF2.pdf"
 doc = fitz.open(pdf_path)
-page_number = 0  # NumÃ©ro de la page (commence Ã  0)
+page_number = 0 
 page = doc.load_page(page_number)
 text = page.get_text("text")
 print(text)
