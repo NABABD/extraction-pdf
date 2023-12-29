7c9e8464fd0a5811bf770c4e126268f25e1166cd
NABABD <nababd47@gmail.com>
2023-12-29 12:56:23 +0100

    recuperation des données chiffrés pour les params

--- convertiseur.py
+++ convertiseur.py
@@ -1,14 +1,9 @@
 import fitz  # PyMuPDF
 import re
-pdf_path = "PDF2.pdf"
-doc = fitz.open(pdf_path)
-page_number = 0 
-page = doc.load_page(page_number)
-text = page.get_text("text")
-print(text)
 
 
-def extraction(chemin):
+# extraire toutes les données du fichier et le mettre dans la variable texte
+def extraction(chemin): 
     text = ""
     with fitz.open(chemin) as pdf_document:
         for page_num in range(pdf_document.page_count):
@@ -21,17 +16,62 @@ def expMatches(text, regex_pattern):
     matches = re.findall(regex_pattern, text)
     return matches
 
-# Chemin du fichier PDF d'exemple
+def tester_type(motif):
+    motif_chiffres = r'^-?\d+.+$'
+    motif_lettres = r'^[a-zA-Z]+$'
+    motif_alphanumerique = r'^[a-zA-Z0-9]+$'
+    motif_num_special = r'^[a-zA-Z0-9%]+$'
+
+    if re.match(motif_chiffres, motif):
+        return 0 #motif numerique
+    elif re.match(motif_lettres, motif):
+        return 1 #motif uniquement alphabetique
+    elif re.match(motif_alphanumerique, motif):
+        return 2 #motif chiffre et lettre
+    elif re.match(motif_num_special, motif):
+        return 3 #motif sans lettres
+    else:
+        return 4 #motif avec caracteres speciaux
+
+
 chemin = 'PDF2.pdf'
 
-# Extraire le texte du PDF avec PyMuPDF
 pdf_text = extraction(chemin)
 
 # Définir le motif d'expression régulière que vous souhaitez rechercher
-regex_pattern = r'\b(VEMS.*)\b' # Exemple: correspondance à un code postal de 5 chiffres
+regex_pattern = r'\b(VEMS)\b'
 
 # Trouver les correspondances d'expression régulière dans le texte extrait
 regex_matches = expMatches(pdf_text, regex_pattern)
 
-# Afficher les correspondances trouvées
+
 print("Correspondances d'expression régulière trouvées:", regex_matches)
+
+
+# Exemples de tests
+print(tester_type("-0.62") ) 
+
+
+
+def DonneeDunParametre(parametre):
+    liste_param=[]
+    motif = fr'{parametre}\s+(.+)'
+    correspondance = re.search(motif, pdf_text)
+    donnees_apres = correspondance.group(1)
+    liste_param.append(donnees_apres)
+    while tester_type(donnees_apres)==0:  #tant que la données suivant est une valeur pour le parametre on continue
+        motif=fr'{donnees_apres}\s+(.+)'
+        correspondance = re.search(motif, pdf_text)
+        if correspondance:
+            donnees_apres = correspondance.group(1)
+            if tester_type(donnees_apres)==0:
+                liste_param.append(donnees_apres)
+
+
+    return liste_param
+print("//////////////////////////////////////////////")
+print(DonneeDunParametre("VT"))
+print(DonneeDunParametre("VEMS"))
+print(DonneeDunParametre("CVF"))
+
+
